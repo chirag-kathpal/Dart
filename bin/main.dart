80378fd3ba89c46322f8ef4290637baa66bc7aaa
Chirag <chiragkath7@gmail.com>
2023-04-12 22:35:40 +0530

    Basics

---
+++ bin/main.dart
@@ -0,0 +1,36 @@
+void main() {
+  // Used to print any object
+  print("Hello Dart");
+
+  // Variables
+  var f_name = "Chirag";
+  var l_name = " Kathpal";
+  print(f_name + l_name);
+
+  // Built-in-Datatypes
+  int item = 1;
+  double price = 99.9;
+  String city = "Fatehabad";
+  bool is_right = true;
+  var quantity = 100.5; //Dynamic data type
+  print(quantity.runtimeType); //check data type of a variable
+
+  // # Operators
+  // ## Arithmetic operators
+  var a = 3;
+  var b = 10;
+  print(a + b);
+  print(b - a);
+  print(a * b);
+  print(b / a);
+  print(b ~/ a);
+  print(b % a);
+
+  // ## prefix and postfix incremet and decrement operators
+  var i = 3;
+  print(++i);
+  print(i--);
+
+  // ## Equality and Relational operator
+  
+}
