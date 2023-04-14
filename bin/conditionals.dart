d597cf36df38bcff7f3685f076a0048753324ac4
Chirag <chiragkath7@gmail.com>
2023-04-14 12:34:38 +0530

    Conditionals

--- bin/conditionals.dart
+++ bin/conditionals.dart
@@ -2,12 +2,18 @@ void main() {
   // #Conditional Expression
   var is_login = true;
 
-  // condition ? exprsn1 : eprsn2 ;
+  // ## condition ? exprsn1 : eprsn2 ;
   var user = is_login ? 'Chirag' : 'Guest';
   print(user);
 
   // var name = 'Chirag';
   var name = null;
   var ans = name ?? 'Guest';
-  // if not null print variable ,otherwise print the expression
+  // ## if not null print variable ,otherwise print the expression
+
+  // # Control Flow Statements
+  // ## if statement
+  // ##if else
+  // ## if else if
+
 }
