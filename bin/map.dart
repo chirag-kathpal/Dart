05779575a32342f87f8f89574b91f98990a8021e
Chirag <chiragkath7@gmail.com>
2023-04-14 12:07:52 +0530

    map defination

---
+++ bin/map.dart
@@ -0,0 +1,9 @@
+void main() {
+  // # Map - Dynamic Collection of key-value pair
+  // ## Map Literal
+  var address = {'name': 'rahul', 'city': 'Ranchi', 'State': 'Jharkhand'};
+  print(address);
+  print(address.runtimeType);
+
+  
+}
