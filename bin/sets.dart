6ccf44ee58dd6c03ba55a5e66f0832562997b23e
Chirag <chiragkath7@gmail.com>
2023-04-14 11:21:54 +0530

    ways to define set

--- bin/sets.dart
+++ bin/sets.dart
@@ -3,4 +3,17 @@ void main() {
   var st1 = {'Rahul', 'Sonam', 'Sumit'};
   print(st1);
   print(st1.runtimeType);
+
+  var st2 = <int>{1, 2, 3, 4};
+  print(st2);
+  print(st2.runtimeType);
+
+  Set<String> st3 = {'aa', 'bb'};
+  print(st3);
+  print(st3.runtimeType);
+
+  // ## Empty Set
+
+  var st4 = <int>{};
+  print(st4.runtimeType);
 }
