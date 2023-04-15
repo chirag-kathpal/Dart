648bd5da467540852693792067de2daf8fb19467
Chirag <chiragkath7@gmail.com>
2023-04-15 17:53:21 +0530

    Classes and Objects

--- bin/clsandobj.dart
+++ bin/clsandobj.dart
@@ -1,3 +1,29 @@
+// #Class and Object
+class Class_name {
+  // class members
+}
+
 void main() {
-  
+  // Creating an object
+  var oppo = new Mobile();
+
+  // calling instance method using object
+  oppo.showModel("f1");
+
+  var realme = Mobile();
+  realme.showModel("X2");
+  print(oppo.ram);
+}
+
+class Mobile {
+  // instance variable
+  late String model;
+  late int rank;
+  int ram = 8;
+
+  // Instance method
+  showModel(md) {
+    model = md;
+    print(model);
+  }
 }
