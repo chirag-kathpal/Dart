b030eb720df8ecba1e1225662dbd39a5438749a1
Chirag <chiragkath7@gmail.com>
2023-04-15 17:32:09 +0530

    Functions Complete

--- bin/functions.dart
+++ bin/functions.dart
@@ -1,3 +1,51 @@
 void main() {
   // # Functions
+  // Functiona are also object in Dart
+  myFun() {
+    print("Hello dart");
+  }
+
+  // myFun();
+
+  addFun() {
+    int a = 10;
+    int b = 20;
+    return a + b;
+  }
+
+  // print(addFun());
+
+  int sum(int a, int b) {
+    return a + b;
+  }
+
+  int res = sum(4, 5);
+  // print(res);
+
+  // optional positional parameters
+
+  // default value parameter
+  String intro(String name, [String city = 'Delhi']) {
+    return 'I am $name from $city';
+  }
+
+  // print(intro('Sonam','Mumbai'));
+
+  // Named parammeters
+  addFunc({a, b}) {
+    print(a);
+    print(b);
+  }
+
+  // addFunc(b: 10, a: 20);
+
+  // Anonymous Functions
+  var student = ['sonam', 'rahul', 'sumit'];
+  student.forEach((element) {
+    print(element);
+  });
+
+  // Arrow Functions
+  student.forEach((element)=>print(element));
+
 }
