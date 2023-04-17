20d974a86e3b6392f032183df692277fc80954be
Chirag <chiragkath7@gmail.com>
2023-04-17 18:01:26 +0530

    Implicit interface

---
+++ bin/implicit_interface.dart
@@ -0,0 +1,34 @@
+import 'inheritance.dart';
+
+void main() {
+  // implicit interface - To achieve multiple inheritance
+  var obj = Student();
+  obj.disp();
+  obj.disp2();
+}
+
+class Teacher {
+  disp() {
+    print('I am Teacher');
+  }
+}
+
+class Teacher2 {
+  disp2() {
+    print('I am Teacher2');
+  }
+}
+
+class Student implements Teacher, Teacher2 {
+  // TODO: implement disp
+  @override
+  disp() {
+    print('I am Student');
+  }
+
+  // TODO: implement disp2
+  @override
+  disp2() {
+    print('I am Student');
+  }
+}
