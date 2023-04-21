9e2d3ffd18759519bfe2d0ec3ec993c2f78e2c8b
Chirag Kathpal <chiragkath7@gmail.com>
2023-04-21 16:23:34 +0530

    const constructors

--- bin/constAndFinal.dart
+++ bin/constAndFinal.dart
@@ -4,7 +4,17 @@ void main() {
 
   // myConstNumber = 42; // error
   // myFinalNumber = 42; // error
+
+  const constVariable = 123456;
+  // const constructors
+  const x = MyConstClass(constVariable, ['hello', 'there']);
 }
 // final is additionally suited for more occasions
 // while const can be used only for top-level,
 // static or local variables. That means no const instance fields.
+
+class MyConstClass {
+  final int field1;
+  final List<String> field2;
+  const MyConstClass(this.field1, this.field2);
+}
