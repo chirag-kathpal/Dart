4c0ce734a5a3522a19cbc79f689da6e157ffdaa0
Chirag Kathpal <chiragkath7@gmail.com>
2023-04-24 12:54:50 +0530

    stream

--- bin/streams.dart
+++ bin/streams.dart
@@ -1,3 +1,22 @@
-void main() {
+void main() async{
+  print("main has started");
+  var test = asyncGenerator(5);
+  print(test.runtimeType);
 
+  var sub = test.listen((event) {
+    print(event);
+  });
+
+  await Future.delayed(Duration(seconds: 6));
+  sub.cancel();
+  print(sub.runtimeType);
+}
+
+Stream<int> asyncGenerator(int count) async* {
+  int k = 0;
+
+  while (k < count) {
+    await Future.delayed(Duration(seconds: 1));
+    yield k++;
+  }
 }
