3ef3f0bc1d95f1ce1877c1083e3647402b68a738
Chirag Kathpal <chiragkath7@gmail.com>
2023-04-24 12:33:02 +0530

    Futures in dart

---
+++ bin/futures_more.dart
@@ -0,0 +1,29 @@
+// Generic types
+// Future<T>
+// Stream<T>
+// StreamSubscription<T>
+void main() async {
+  print("main has started");
+  // await printAfterDelay(1);
+  // await printAfterDelay(2);
+  // await printAfterDelay(3);
+  // await printAfterDelay(4);
+  // await printAfterDelay(5);
+  try {
+    var test = await printAfterTime(1);
+  } catch (e) {
+    print(e);
+  }
+  print("main has ended");
+}
+
+Future<void> printAfterDelay(int seconds) async {
+  await Future.delayed(Duration(seconds: seconds));
+  print("Delayed $seconds seconds");
+}
+
+Future<int> printAfterTime(int seconds) async {
+  await Future.delayed(Duration(seconds: seconds));
+  print("Delayed $seconds seconds");
+  return seconds;
+}
